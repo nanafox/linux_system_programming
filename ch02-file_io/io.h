0af851ac18c8910f11ac3daf4bfb96be5e915dc1
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-09-28 14:29:23 +0000

    Updated to include necessary header files - with an extra function

--- ch02-file_io/io.h
+++ ch02-file_io/io.h
@@ -1,7 +1,13 @@
 #ifndef __IO_H__
 #define __IO_H__
 
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
 int _creat(const char *, int);
 int mkfile(const char *, int);
+void check_fd(int fd, int event);
 
 #endif
