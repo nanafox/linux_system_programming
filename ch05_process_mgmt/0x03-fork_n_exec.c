b5d370e35c840543db21bf18d8b60ce78171ae56
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2023-11-01 12:26:43 +0000

    Added a C program to create new process and execute programs

---
+++ ch05_process_mgmt/0x03-fork_n_exec.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+/**
+ * main - executes a program
+ * @argc: the argument counter
+ * @argv: the argument vector
+ *
+ * Description: This program spawns a new process (child process) and runs
+ * whatever command the shell gives on the command line if it's a valid command
+ * else it fails. The command can take any number of arguments so long as it's
+ * valid with the command being executed.
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int main(int argc, char *argv[])
+{
+	char path[30];
+	int retval;
+	pid_t pid;
+
+	if (!(argc >= 2))
+	{
+		fprintf(stderr, "Usage: %s <program_name> [args...]\n", argv[0]);
+		return (-1);
+	}
+
+	pid = fork(); /* create a child process */
+	if (pid == -1)
+	{
+		perror("fork");
+		return (-1);
+	}
+
+	/* ensure we are in the child process */
+	if (!pid)
+	{
+		sprintf(path, "/bin/%s", argv[1]); /* build up the absolute path */
+		retval = execve(path, &argv[1], NULL); /* execute the program */
+
+		if (retval == -1)
+		{
+			perror("run");
+			return (-1); /* an error occured while trying to execute the program */
+		}
+	}
+
+	return (0);
+}
